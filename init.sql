9ae26cf00d870dfd1a30b6be4822c8ffe574598e
Haldun Matar <61618861+HaldunMatar@users.noreply.github.com>
2024-09-01 19:25:26 +0300

    first commit

---
+++ init.sql
@@ -0,0 +1,15 @@
+
+
+CREATE DATABASE IF NOT EXISTS madb;
+CREATE USER IF NOT EXISTS 'dba'@'%' IDENTIFIED BY 'Password123$';
+GRANT ALL PRIVILEGES ON madb.* TO 'dba'@'%';
+FLUSH PRIVILEGES;
+
+CREATE TABLE users ( id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), email VARCHAR(100) );
+INSERT INTO users (name, email) VALUES ('ali', 'ali@example.com');
+INSERT INTO users (name, email) VALUES ('haldun', 'haldun@techniaa.com');
+USE madb ; 
+
+-- Debugging lines
+SELECT User, Host FROM mysql.user;
+SHOW GRANTS FOR 'dba'@'%';
