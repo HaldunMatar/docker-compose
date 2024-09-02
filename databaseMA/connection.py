7bf39507ea8f96cbcd1cb6fe9555ad709755e7af
Haldun Matar <61618861+HaldunMatar@users.noreply.github.com>
2024-09-02 18:29:45 +0300

    dba

--- databaseMA/connection.py
+++ databaseMA/connection.py
@@ -36,11 +36,108 @@ def get_connection():
    
     host_name= get_host_name()
     
-   
-    if 1==1:
+    # if host_name=='Halduns-MacBook-Pro.local':
+    # if host_name=='Halduns-MacBook-Pro.local':
+    if(1==1)  :  
         print('get_connection test' , ' hostname ',host_name )
         if(not is_docker):
             host='127.0.0.1'
         database='MADB'
         password='12345678'
         user='root'                                  
+    elif host_name=='inas':
+        if(not is_docker):
+            host='127.0.0.1'
+        database='madb'
+        password=''
+        user='root'
+ 
+    elif host_name=='mainsight-desktop':
+        if(not is_docker):
+            host='100.103.105.34'
+        database='madb'
+        password='Password123$'
+        user='dba'
+
+    elif host_name=='anas-desktop':  
+        if(not is_docker):          
+            host='100.74.149.25'
+        database='MADB'
+        password='Password123$'
+        user='dba'
+
+    elif host_name=='gama':
+        if(not is_docker):        
+            host='100.66.253.61'
+        database='madb'
+        password='Password123$'
+        user='dba'
+                                                 
+ 
+    elif host_name=='delta':
+        if(not is_docker):         
+            host='100.69.135.43'
+        host='100.66.253.61'
+        database='madb'
+        password='Password123$'
+        user='dba'    
+    
+    
+    #print('getConnection ', host ,database ,password ,user  )                                  
+               
+    return mysql.connector.connect(
+        
+                host=host,
+                database=database,
+                password=password,
+                user=user                               
+                )          
+
+
+
+
+
+
+
+     
+#36
+# connection.get_connection() = mysql.connector.connect(
+#     host=ipDBsever,
+#     database='MADB',
+#     password='123',
+#     user='root'                                       
+#     )
+
+# local
+# connectionDB = mysql.connector.connect(
+#     #host='100.74.149.25',
+#     host=ipDBsever,
+
+#                 #     database='MADB',
+#                 # password='Password123$',
+#                 # user='dba'   
+#     database='MADB',
+#     password='12345678',
+#     user='root'                                       
+#     )
+
+# connection.get_connection() = mysql.connector.connect(
+#                     host='localhost',
+#                     user='root',
+#                     password='',
+#                     database='test'
+# beta (test)                )
+# connection.get_connection() = mysql.connector.connect(
+#     host=ipDBsever,
+#     database='MADB',
+#     password='Password123$',
+#     user='dba'                                      
+#     )
+
+#alpha                
+# connection.get_connection() = mysql.connector.connect(
+#     host=ipDBsever,
+#     database='MADB',
+#     password='Password123$',
+#     user='dba'                                      
+#     )
